17d27c695aac52dbc2c67ffc464d22e8295c61c8
Luc Abayo <luc.bayo@gmail.com>
2020-09-02 16:30:40 +0200

    feature: add image upload (#8)

--- src/api/admin/products.js
+++ src/api/admin/products.js
@@ -16,23 +16,30 @@ export default crud("/products", Product, {
     Product.findByPk(id, {
       include,
     }),
-  create: (body) => {
-    return Product.create(
-      {
-        ...body,
-      },
-      {
-        include,
-      }
-    );
+  create: async (body) => {
+    const product = await Product.create({
+      ...body,
+      files: undefined,
+    });
+    appEvents.emit("upload_product_image", {
+      productId: product.id,
+      files: body.files,
+    });
+    return product;
   },
   update: async (body, options) => {
     const product = await Product.findOne({ ...options, include });
     if (!product) throw new Error(`Product doesn't exist`);
-    const productAttributes = { ...body, images: undefined };
+    const productAttributes = { ...body, images: undefined, files: undefined };
     product.set(productAttributes);
-    await product.save();
+
     appEvents.emit("update_product_image", { product, images: body.images });
+    appEvents.emit("upload_product_image", {
+      productId: product.id,
+      files: body.files,
+    });
+
+    await product.save();
     return product;
   },
 });
--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import registerEvents from "./middlewares/registerEvents";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 passport.use(passportJwt);
 
 registerEvents();
