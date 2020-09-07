e06dc444321537e0a7013a08b851d6c0d09d4433
Luc <luc.bayo@gmail.com>
2020-09-07 19:41:02 +0200

    feature: add pagination

--- src/api/v1/products/controller.js
+++ src/api/v1/products/controller.js
@@ -1,13 +1,19 @@
 const { Category, Review, Sequelize } = require("../../../models");
 import { productModels } from "../../../constants/associatedModels";
+import { paginate, textSearch } from "../../../utils/queryHelpers";
 
 export const allProductsQuery = (req, res, next) => {
+  const { search, page = 1, limit = 25, companyId } = req.query;
   req.modelQuery = {
     include: productModels,
+    where: {
+      ...textSearch(search, ["name", "fullDescription", "shortDescription"]),
+    },
     order: [
       ["createdAt", "DESC"],
       ["name", "ASC"],
     ],
+    ...paginate({ page, limit }),
   };
   return next();
 };
