8f1849f056c79c1215d116e185c30ff69c2c8985
Luc <luc.bayo@gmail.com>
2020-09-07 19:53:13 +0200

    feature: search products

--- src/api/v1/products/controller.js
+++ src/api/v1/products/controller.js
@@ -1,11 +1,35 @@
-const { Category, Review, Sequelize } = require("../../../models");
+const { Category, Review, Image } = require("../../../models");
+import { Op } from "sequelize";
 import { productModels } from "../../../constants/associatedModels";
 import { paginate, textSearch } from "../../../utils/queryHelpers";
 
 export const allProductsQuery = (req, res, next) => {
-  const { search, page = 1, limit = 25, companyId } = req.query;
+  const { search, page = 1, limit = 25, category } = req.query;
   req.modelQuery = {
-    include: productModels,
+    include: [
+      {
+        model: Category,
+        as: "categories",
+        through: { attributes: [] },
+        where: {
+          ...(category
+            ? {
+                name: {
+                  [Op.iLike]: `%${category || search}%`,
+                },
+              }
+            : {}),
+        },
+      },
+      {
+        model: Review,
+        as: "reviews",
+      },
+      {
+        model: Image,
+        as: "images",
+      },
+    ],
     where: {
       ...textSearch(search, ["name", "fullDescription", "shortDescription"]),
     },
